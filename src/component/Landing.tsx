d9105302a088c93f8e85bbd335c8f2bf58370062
Alaa <alaaali@meltwater.org>
2023-04-12 20:45:47 +0200

    Edit landing page

--- src/component/Landing.tsx
+++ src/component/Landing.tsx
@@ -12,6 +12,7 @@ import {
   Flex,
   VStack,
   HStack,
+  Image,
   Center,
 } from "@chakra-ui/react";
 import NavBar from "@/component/NavBar";
@@ -43,6 +44,24 @@ export default function Landing() {
           bgRepeat="no-repeat"
         >
           <NavBar />
+
+          <Text
+            marginStart={"30%"}
+            fontSize="6xl"
+            mt={"10vh"}
+            fontWeight="bold"
+            fontFamily={"var(--font-fraunces)"}
+            color="white"
+          >
+            WE ARE CREATIVES
+          </Text>
+
+          <Image
+            mt={"5vh"}
+            marginStart={"50%"}
+            src="images/icon-arrow-down.svg"
+            alt="Dan Abramov"
+          />
         </Box>
 
         <VStack align="stretch">
@@ -50,12 +69,6 @@ export default function Landing() {
             <Box
               w="50%"
               h="100vh"
-              // bgImage={firstImage}
-              // bgSize="cover"
-              // bgAttachment="semi-fixed"
-              // bgPos="50% 100%"
-              // pos="relative"
-              // bgRepeat="no-repeat"
               justifyContent={"center"}
               alignItems="center"
             >
@@ -64,6 +77,7 @@ export default function Landing() {
                 mt={"20vh"}
                 marginInline={"20vh"}
                 fontWeight="bold"
+                fontFamily={"var(--font-fraunces)"}
               >
                 Trasform your brand
               </Text>
@@ -72,6 +86,7 @@ export default function Landing() {
                 fontWeight="bold"
                 mt={"5vh"}
                 marginInline={"20vh"}
+                fontFamily={"var(--font-barlow)"}
                 noOfLines={3}
               >
                 We are a full-service creative agency specializing in helping{" "}
@@ -113,12 +128,6 @@ export default function Landing() {
             <Box
               w="50%"
               h="100vh"
-              // bgImage={firstImage}
-              // bgSize="cover"
-              // bgAttachment="semi-fixed"
-              // bgPos="50% 100%"
-              // pos="relative"
-              // bgRepeat="no-repeat"
               justifyContent={"center"}
               alignItems="center"
             >
@@ -127,6 +136,7 @@ export default function Landing() {
                 mt={"20vh"}
                 marginInline={"20vh"}
                 fontWeight="bold"
+                fontFamily={"var(--font-fraunces)"}
               >
                 Stand out to the right audience
               </Text>
@@ -136,6 +146,7 @@ export default function Landing() {
                 mt={"5vh"}
                 marginInline={"20vh"}
                 noOfLines={3}
+                fontFamily={"var(--font-barlow)"}
               >
                 Using a collaborative formula of designers, researchers,{" "}
                 photographers, videographers, and copywriters, well build and
@@ -160,28 +171,29 @@ export default function Landing() {
               bgAttachment="semi-fixed"
               bgPos="50% 100%"
               pos="relative"
-              justifyItems={"center"}
-              justifyContent="center"
               bgRepeat="no-repeat"
             >
               <Text
                 fontSize="6xl"
                 mt={"60vh"}
-                marginInline={"20vh"}
+                marginInline={"15vh"}
                 fontWeight="bold"
+                fontFamily={"var(--font-fraunces)"}
               >
                 Graphic design
               </Text>
+
               <Text
                 fontSize="1xl"
                 fontWeight="bold"
-                mt={"10vh"}
+                mt={"5vh"}
                 marginInline={"20vh"}
                 noOfLines={3}
+                fontFamily={"var(--font-barlow)"}
               >
-                Great design makes you memorable. We deliver artwork that
-                underscores your brand message and captures potential clients’
-                attention.
+                Using a collaborative formula of designers, researchers,{" "}
+                photographers, videographers, and copywriters, well build and
+                extend your brand in digital places.
               </Text>
             </Box>
             <Box
@@ -198,7 +210,7 @@ export default function Landing() {
               <Text
                 fontSize="6xl"
                 mt={"60vh"}
-                fontFamily={"var(--font-barlow)"}
+                fontFamily={"var(--font-fraunces)"}
                 marginInline={"20vh"}
                 fontWeight="bold"
               >
@@ -241,16 +253,3 @@ export default function Landing() {
     </>
   );
 }
-
-const Arrow = createIcon({
-  displayName: "Arrow",
-  viewBox: "0 0 72 24",
-  path: (
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M0.600904 7.08166C0.764293 6.8879 1.01492 6.79004 1.26654 6.82177C2.83216 7.01918 5.20326 7.24581 7.54543 7.23964C9.92491 7.23338 12.1351 6.98464 13.4704 6.32142C13.84 6.13785 14.2885 6.28805 14.4722 6.65692C14.6559 7.02578 14.5052 7.47362 14.1356 7.6572C12.4625 8.48822 9.94063 8.72541 7.54852 8.7317C5.67514 8.73663 3.79547 8.5985 2.29921 8.44247C2.80955 9.59638 3.50943 10.6396 4.24665 11.7384C4.39435 11.9585 4.54354 12.1809 4.69301 12.4068C5.79543 14.0733 6.88128 15.8995 7.1179 18.2636C7.15893 18.6735 6.85928 19.0393 6.4486 19.0805C6.03792 19.1217 5.67174 18.8227 5.6307 18.4128C5.43271 16.4346 4.52957 14.868 3.4457 13.2296C3.3058 13.0181 3.16221 12.8046 3.01684 12.5885C2.05899 11.1646 1.02372 9.62564 0.457909 7.78069C0.383671 7.53862 0.437515 7.27541 0.600904 7.08166ZM5.52039 10.2248C5.77662 9.90161 6.24663 9.84687 6.57018 10.1025C16.4834 17.9344 29.9158 22.4064 42.0781 21.4773C54.1988 20.5514 65.0339 14.2748 69.9746 0.584299C70.1145 0.196597 70.5427 -0.0046455 70.931 0.134813C71.3193 0.274276 71.5206 0.70162 71.3807 1.08932C66.2105 15.4159 54.8056 22.0014 42.1913 22.965C29.6185 23.9254 15.8207 19.3142 5.64226 11.2727C5.31871 11.0171 5.26415 10.5479 5.52039 10.2248Z"
-      fill="currentColor"
-    />
-  ),
-});
